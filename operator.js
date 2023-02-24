04e7fa0d078d628242a7fe8182e5979de3a50c98
Prachee_garg <gargprachi1882@gmail.com>
2023-02-24 15:29:25 +0530

    first commit

---
+++ operator.js
@@ -0,0 +1,68 @@
+
+
+// if we have to print 10 numbers
+
+
+
+/*console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);*/
+
+
+
+
+
+
+
+//another way
+
+//in every step i is incremented by 1
+
+// that is i++
+
+/*let i=0;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+i++;
+console.log(i);
+console.log(i);*/
+//but this make the program so lengthy..
+
+
+
+
+
+
+
+let i=0;
+while(i<10)
+{
+    console.log(i);
+    i++;
+}
+
+console.log(`current value of i is ${i}`);
